7dd5f28c0ea8fbee1bee8b51dacfe90a0de29dcd
shygosh <shygosh@proton.me>
2025-05-04 15:56:12 +0300

    sched/fair: Always inline reweight_task()
    Signed-off-by: shygosh <shygosh@proton.me>
    [vbajs]
    * Adopt to backslashxx's backport of reweight_task() https://github.com/backslashxx/mojito_krenol/commit/462d56ec5dd5d2eaea4e4879587c840daa64812b
    
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1827,6 +1827,8 @@ extern void init_sched_dl_class(void);
 extern void init_sched_rt_class(void);
 extern void init_sched_fair_class(void);
 
+extern __always_inline void reweight_task(struct task_struct *p, int prio);
+
 extern void resched_curr(struct rq *rq);
 extern void resched_cpu(int cpu);
 
